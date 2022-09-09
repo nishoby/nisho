695f4759dfdcad03424141521c02e65f06979b4e
Alherd <algerd.blr@gmail.com>
2022-09-09 22:57:00 +0300

    Initial template

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,67 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div class="header-wrp fixedhrd">
+        <div class="header flex-row flex-algn-itms-c">
+            <a href="#" class="section pdng-l-20px pdng-r-20px">
+                <img src="/img/woman.png" width="200" height="150">
+            </a>
+            <div class="header-links flex-grow-all pdng-l-20px pdng-r-20px mil-notdisplay">
+                <a href="">Аб праекце</a>
+                <a href=""></a>
+            </div>
+            <!-- mobile nav -->
+            <div class="section flex-grow-all pdng-l-20px pdng-r-30px notdisplay mil-show">
+                <input id="brgrbtn" class="notdisplay mil-show" type="checkbox">
+                <label for="brgrbtn" class="notdisplay burger-button mil-show">
+                    <div class="burger-button-line"></div>
+                    <div class="burger-button-line"></div>
+                    <div class="burger-button-line"></div>
+                </label>
+                <div class="brgr-nav notdisplay mil-show">
+                    <div class="header-links pdng-l-20px pdng-r-20px">
+                        <div class="pdng-t-30px">
+                            <a href="#">Выборы</a>
+                        </div>
+                        <div class="pdng-t-30px">
+                            <a href="#">Нарушения</a>
+                        </div>
+                        <div class="pdng-t-30px">
+                            <a href="#">Комиссии</a>
+                        </div>
+                        <div class="pdng-t-30px">
+                            <a href="#">Организации</a>
+                        </div>
+                        <div class="pdng-t-30px">
+                            <a class="active" href="#">Новости</a>
+                        </div>
+                        <div class="pdng-t-30px">
+                            <a href="#">О проекте</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-subnav border-color2">
+            <div class="section pdng-30px pdng-t-15px pdng-b-15px mil-pdng-20px mil-pdng-t-10px mil-pdng-b-10px flex-grow-all">
+                <div class="inline-block">
+                    <div class="txt-size-12px txt-color-3-1 mrgn-b-5px">
+                        Поиск
+                    </div>
+                    <div class="section pdng-r-30px">
+                        <div class="search-input-wrp">
+                            <input class="search-input" type="search" placeholder="Поиск">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="scene mrgn-t-170px mil-mrgn-t-120px">
+    </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
