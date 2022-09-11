10c55c437d388be8d6a8e89d53bf88d8978b8895
Alherd <algerd.blr@gmail.com>
2022-09-11 13:00:29 +0300

    Added sentry

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import {createApp} from 'vue'
+import {createApp}      from 'vue'
 import './assets/main.css'
-import App         from './App.vue'
-import router      from "./router";
+import App              from './App.vue'
+import router           from "./router";
+import * as Sentry      from "@sentry/vue";
+import {BrowserTracing} from "@sentry/tracing";
 
 const app = createApp(App)
+
+if (import.meta.env.MODE !== 'development') {
+    Sentry.init({
+        app,
+        dsn         : import.meta.env.VITE_SENTRY_DSN,
+        integrations: [
+            new BrowserTracing({
+                routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+                tracingOrigins        : ["localhost", "nisho.netlify.app", /^\//],
+            }),
+        ],
+        tracesSampleRate: 1.0,
+    });
+}
+
+
 app.use(router)
 app.mount('#app')
