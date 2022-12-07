0f55bcebc273a7f789f03a4f760d5b498426a2ba
Alherd <algerd.blr@gmail.com>
2022-12-07 17:53:01 +0200

    Unused import

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import {createApp}      from 'vue'
-import './assets/main.css'
 import App              from './App.vue'
 import router           from "./router";
 import * as Sentry      from "@sentry/vue";
