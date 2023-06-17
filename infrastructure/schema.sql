fd42356982de17bd627a62155b011f13511a7c3d
Alherd <algerd.blr@gmail.com>
2023-06-17 08:37:34 +0200

    Fixes #25 when you can't add existing tags to definition on adding/editing

--- infrastructure/schema.sql
+++ infrastructure/schema.sql
@@ -1,19 +1,41 @@
-create function add_term(term_name character varying, definition character varying,
-                         example character varying) returns integer
-    language sql as
+create function add_term(term_name character varying, definition character varying, example character varying, tags character varying[]) returns integer
+    language sql
+    as
 $$
-insert into term (name, user_id)
-select term_name,
-    auth.uid() where not exists (
-  select 1 from term where lower(name) = lower(term_name)
-);
-INSERT INTO definition (content, term_id, user_id, example)
-SELECT definition,
-    id as term_id,
-    auth.uid(),
-    example
-FROM term
-where lower(name) = lower(term_name) returning term_id
+with insert_term as (
+    insert into term(name, user_id)
+        select term_name,
+            auth.uid()
+        where not exists(
+          select 1 from term where lower(name) = lower(term_name)
+            ) returning id
+),
+    result_term as (
+        SELECT id FROM insert_term
+        UNION
+        SELECT id FROM term WHERE lower(name) = lower(term_name)
+    ),
+    insert_def as (
+        INSERT INTO definition(content, term_id, user_id, example)
+            VALUES (definition,
+                (SELECT id FROM result_term),
+                auth.uid(),
+                example)
+            returning id
+    ),
+    insert_tag as (
+        insert into tag(name) SELECT n FROM unnest(tags) AS n ON CONFLICT DO NOTHING RETURNING id as tag_id
+    ),
+    select_tag as (
+        SELECT (select id from insert_def) as definition_id, tag_id from insert_tag
+        UNION
+        SELECT (select id from insert_def) as definition_id, id as tag_id from tag WHERE name in (SELECT n FROM unnest(tags) as n)
+    ),
+    insert_dt as (
+        INSERT INTO definition_tag(definition_id, tag_id)
+                    SELECT definition_id, tag_id from select_tag
+    )
+SELECT id FROM result_term;
 $$;
 
 
@@ -118,10 +140,9 @@ create trigger after_user_update
 
 
 
-create function edit_term(definition_id int, definition varchar, example character varying,
-                          tags          character varying[]) returns integer
+create or replace function edit_term(definition_id integer, definition character varying, example character varying, tags character varying[]) returns integer
     language sql
-    as
+as
 $$
 DELETE
 FROM definition_tag as dt
@@ -130,11 +151,24 @@ with list_tags as (
     SELECT tag
     FROM unnest(tags) AS tag
 ),
-    insert_tag as (INSERT INTO tag (name) (SELECT tag from list_tags) on conflict do nothing returning id as tag_id)
-INSERT INTO definition_tag(definition_id, tag_id)
+    insert_tag as (INSERT INTO tag (name) (SELECT tag from list_tags) on conflict do nothing returning id as tag_id),
+    select_tag as (
 SELECT edit_term.definition_id,
     tag_id
-from insert_tag;
+from insert_tag
+UNION
+SELECT edit_term.definition_id, id as tag_id from tag WHERE name IN (SELECT tag from list_tags)
+    )
+INSERT INTO definition_tag(definition_id, tag_id)
+SELECT definition_id, tag_id from select_tag;
 update definition SET content = edit_term.definition, example = edit_term.example WHERE id = definition_id;
 SELECT term_id FROM definition WHERE id = edit_term.definition_id;
 $$;
+
+alter function edit_term(integer, varchar, varchar, character varying[]) owner to postgres;
+
+grant execute on function edit_term(integer, varchar, varchar, character varying[]) to anon;
+
+grant execute on function edit_term(integer, varchar, varchar, character varying[]) to authenticated;
+
+grant execute on function edit_term(integer, varchar, varchar, character varying[]) to service_role;
